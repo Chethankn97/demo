5893746e77155ab9cb02daebc6b2e452078fdef0
Chethankn97 <chethu2407@gmail.com>
2020-05-29 16:17:25 +0530

    first commit adding proj

---
+++ src/main/java/sonartutorial/com/engineer/App.java
@@ -0,0 +1,13 @@
+package sonartutorial.com.engineer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sonartutorial/com/engineer/SimpleTest.java
@@ -0,0 +1,12 @@
+package sonartutorial.com.engineer;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+  @Test
+  public void coveredByUnitTest() {
+	  Simple engineer = new Simple();
+	    engineer.coveredByUnitTest();
+	  }
+	}
